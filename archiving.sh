3082d92bd10716083e93af50b776003a8dc2ec50
Guo Lidong <guolidong@genomics.cn>
2019-07-30 12:51:19 +0800

    update archiving.sh

--- archiving.sh
+++ archiving.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $1 == "h" || $1 == "-h" || $1 == "--help" ]] ; then 
+if [[ $1 == "h" || $1 == "-h" || $1 == "--help" ]] ; then
     echo "usage : run this script in your-working-folder."
     echo "        it will : "
     echo "                 mv output files into Final_Output_TAG. "
@@ -16,7 +16,7 @@ fi
 # Checking working direcory
 #
 #################################
-if [[ ! -f ./profile ]] ; then 
+if [[ ! -f ./profile ]] ; then
     echo "error : no profile found in current folder ! exiting ..."
     echo "info  : $0 -h will print usage !"
     exit 1;
@@ -36,10 +36,10 @@ TMPDIR="Final_TEMP_"$TAG
 mkdir $OUTDIR
 mkdir $TMPDIR
 
-if [[ ! -d $OUTDIR || ! -d $TMPDIR ]] ; then 
+if [[ ! -d $OUTDIR || ! -d $TMPDIR ]] ; then
     echo "error : failed to create archiving folders ! exiting ..."
     echo "info  : $0 -h will print usage !"
-    exit 1 
+    exit 1
 fi
 #################################
 #
@@ -47,9 +47,9 @@ fi
 #
 #################################
 
-mv $PROJECT_NAME"_output" $OUTDIR
+mv $PROJECT_NAME"_output.fasta.gz" $OUTDIR
 mv split_read_stat.log  $OUTDIR
-mv $PROJECT_NAME/outs/report.txt
+mv $PROJECT_NAME/outs/report.txt $OUTDIR
 mv profile $OUTDIR
 
 #################################
@@ -58,15 +58,30 @@ mv profile $OUTDIR
 #
 #################################
 
-mv $R1 $TMPDIR
-mv $R2 $TMPDIR
-mv "$SPLIT""*"  $TMPDIR
+if [[ -f $R1 ]] ; then
+    mv $R1 $TMPDIR
+    mv $R2 $TMPDIR
+fi
+mv $SPLIT".1.fq.gz"  $TMPDIR
+mv $SPLIT".2.fq.gz"  $TMPDIR
+if [[ -f $SPLIT".1.fq.gz.clean.gz" ]] ; then
+    mv $SPLIT".1.fq.gz.clean.gz" $TMPDIR
+    mv $SPLIT".2.fq.gz.clean.gz" $TMPDIR
+fi
 mv $BARCODE_FREQ $TMPDIR
-if [[ -f $CLEAN_BARCODE_FREQ ]] ; then 
+if [[ -f $CLEAN_BARCODE_FREQ ]] ; then
     mv $CLEAN_BARCODE_FREQ $TMPDIR
 fi
+mv read-I1_si-TTCACGCG_lane-001-chunk-001.fastq.gz $TMPDIR
+mv read-R1_si-TTCACGCG_lane-001-chunk-001.fastq.gz $TMPDIR
+mv read-R2_si-TTCACGCG_lane-001-chunk-001.fastq.gz $TMPDIR
+mv sample_S1_L001_I1_001.fastq.gz $TMPDIR
+mv sample_S1_L001_R1_001.fastq.gz $TMPDIR
+mv sample_S1_L001_R2_001.fastq.gz $TMPDIR
 mv $MERGE $TMPDIR
 mv $PROJECT_NAME $TMPDIR
 mv *.log $TMPDIR
 mv *.err $TMPDIR
+mv _step* $TMPDIR
 mv lane.lst $TMPDIR
+
