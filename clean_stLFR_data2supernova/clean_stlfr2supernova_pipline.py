fb004a8c84905df333db54598ab37e3d79ccfefb
Lidong <guolidong@genomics.cn>
2023-02-22 19:20:41 +0800

    Update clean_stlfr2supernova_pipline.py

--- clean_stLFR_data2supernova/clean_stlfr2supernova_pipline.py
+++ clean_stLFR_data2supernova/clean_stlfr2supernova_pipline.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 ###############################################
 # parse parameters
 ###############################################
@@ -10,6 +11,8 @@ parser.add_argument('-supernova', help='supernova soft path', required=True, typ
 parser.add_argument('-o', help='output folder(no exist before)', required=False, type=str)
 parser.add_argument('-f', help='filter less X pair barcode reads(default = 2)', required=False, type=str)
 parser.add_argument('-m', help='mapping ratio (default=8)', required=False, type=str)
+parser.add_argument('-t', help='threads for supernova(default 8)', required=False, type=int)
+parser.add_argument('-M', help='memory(GB) for supernova(default 100)', required=False, type=int)
 parser.add_argument('-s', help='soft path', required=True, type=str)
 args = parser.parse_args()
 # fill default values if necessary
@@ -17,11 +20,22 @@ if args.f:
     filter_num = args.f
 else:
     filter_num = 2
+    
 if args.m:
     mapratio_num = args.m
 else:
     mapratio_num = 8
 
+if args.t:
+    CPU = args.t
+else:
+    CPU = 8
+    
+if args.M:
+    MEM = args.M
+else:
+    MEM = 100
+    
 ###############################################
 # prepare datas and folder
 ###############################################
@@ -54,7 +68,10 @@ os.system('mv read-I1_si-TTCACGCG_lane-001-chunk-001.fastq.gz  supernova_in/samp
 os.system('mv read-R1_si-TTCACGCG_lane-001-chunk-001.fastq.gz  supernova_in/sample_S1_L001_R1_001.fastq.gz')
 os.system('mv read-R2_si-TTCACGCG_lane-001-chunk-001.fastq.gz  supernova_in/sample_S1_L001_R2_001.fastq.gz')
 print('fake over')
+if os.path.exists('./supernova_out'):
+    os.system('mkdir -p prev_run')
+    os.system('mv supernova_out prev_run/supernova_out'+int(time.time())+' ')
 # run supernova 
-os.system(args.supernova + 'supernova run --id=supernova_out --maxreads=2140000000  --fastqs=./supernova_in/ --accept-extreme-coverage --localcores=8 --localmem=500 --nopreflight 1>_log 2>_err')
+os.system(args.supernova + 'supernova run --id=supernova_out --maxreads=2140000000  --fastqs=./supernova_in/ --accept-extreme-coverage --localcores='+ CPU +' --localmem=' + MEM + ' --nopreflight 1>_log 2>_err')
 os.system(args.supernova + 'supernova mkoutput --style=pseudohap --asmdir=supernova_out/outs/assembly --outprefix=xiaoqiang_supernova_result')
 print('pip line over')
