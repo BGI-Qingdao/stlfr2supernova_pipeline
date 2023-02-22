c9ecc2bb88563bd23950aa1d19c8886958bb3776
Lidong <guolidong@genomics.cn>
2023-02-22 18:16:14 +0800

    Update clean_stlfr2supernova_pipline.py

--- clean_stLFR_data2supernova/clean_stlfr2supernova_pipline.py
+++ clean_stLFR_data2supernova/clean_stlfr2supernova_pipline.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+###############################################
+# parse parameters
+###############################################
 parser = argparse.ArgumentParser()
 parser.add_argument('-r1', help='stlfr clean reads1', required=True, type=str)
 parser.add_argument('-r2', help='stlfr clean reads2', required=True, type=str)
@@ -9,16 +12,7 @@ parser.add_argument('-f', help='filter less X pair barcode reads(default = 2)', 
 parser.add_argument('-m', help='mapping ratio (default=8)', required=False, type=str)
 parser.add_argument('-s', help='soft path', required=True, type=str)
 args = parser.parse_args()
-if args.o:
-    os.system('mkdir ' + args.o)
-if args.r1:
-    if '/' in args.r1:
-        os.system('ln -s ' + args.r1 + ' split_reads.1.fq.gz.clean.gz')
-        os.system('ln -s ' + args.r2 + ' split_reads.2.fq.gz.clean.gz')
-    else:
-        os.system('ln -s ' + args.r1 + ' split_reads.1.fq.gz.clean.gz')
-        os.system('ln -s ' + args.r2 + ' split_reads.2.fq.gz.clean.gz')
-os.system('/bin/sh ' + args.s + 'shell_barcode')
+# fill default values if necessary
 if args.f:
     filter_num = args.f
 else:
@@ -27,15 +21,39 @@ if args.m:
     mapratio_num = args.m
 else:
     mapratio_num = 8
+
+###############################################
+# prepare datas and folder
+###############################################
+# check realpath 
+if args.r1:
+    r1path = os.path.realpath(args.r1)
+    r2path = os.path.realpath(args.r2)
+# create folder
+if args.o:
+    os.system('mkdir -p ' + args.o)
+    # switch into output folder now
+    os.chdir(args.o)
+# link input reads
+os.system('ln -s ' + r1path + ' split_reads.1.fq.gz.clean.gz')
+os.system('ln -s ' + r2path + ' split_reads.1.fq.gz.clean.gz')
+# whitelist in supernova folder
 wl = args.supernova + '/supernova-cs/*/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt'
+###############################################
+# main pipeline
+###############################################
+# generate barcode.freq     
+os.system('/bin/sh ' + args.s + 'shell_barcode')
+# generate barcode mapping table     
 os.system('perl ' + args.s + 'merge_barcodes.pl barcode_clean_freq.txt ' + wl + ' merge.txt ' + str(filter_num) + ' ' + str(mapratio_num) +' 1> merge_barcode.log  2>merge_barcode.err')
+# convert file format     
 os.system('perl ' + args.s + 'fake_10x.pl ' + args.r1 + ' ' + args.r2 + ' merge.txt >fake_10X.log 2>fake_10X.err')
-os.system('mv read-I1_si-TTCACGCG_lane-001-chunk-001.fastq.gz ' + args.o + '/sample_S1_L001_I1_001.fastq.gz')
-os.system('mv read-R1_si-TTCACGCG_lane-001-chunk-001.fastq.gz ' + args.o + '/sample_S1_L001_R1_001.fastq.gz')
-os.system('mv read-R2_si-TTCACGCG_lane-001-chunk-001.fastq.gz ' + args.o + '/sample_S1_L001_R2_001.fastq.gz')
-os.system('mv *.log ' + args.o)
-os.system('mv *.err ' + args.o)
+# rename fastqs 
+os.system('mv read-I1_si-TTCACGCG_lane-001-chunk-001.fastq.gz  sample_S1_L001_I1_001.fastq.gz')
+os.system('mv read-R1_si-TTCACGCG_lane-001-chunk-001.fastq.gz  sample_S1_L001_R1_001.fastq.gz')
+os.system('mv read-R2_si-TTCACGCG_lane-001-chunk-001.fastq.gz  sample_S1_L001_R2_001.fastq.gz')
 print('fake over')
-os.system(args.supernova + 'supernova run --id=supernova_out --maxreads=2140000000  --fastqs=' + args.o + ' --accept-extreme-coverage --localcores=8 --localmem=500 --nopreflight 1>_log 2>_err')
+# run supernova 
+os.system(args.supernova + 'supernova run --id=supernova_out --maxreads=2140000000  --fastqs=./ --accept-extreme-coverage --localcores=8 --localmem=500 --nopreflight 1>_log 2>_err')
 os.system(args.supernova + 'supernova mkoutput --style=pseudohap --asmdir=supernova_out/outs/assembly --outprefix=xiaoqiang_supernova_result')
 print('pip line over')
